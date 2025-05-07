e6ea353c366a2e9521cea2aaef382b2bd1424ac0
Elena-ee <elz002@ucsd.edu>
2025-05-07 04:42:39 -0700

    implement Speech Synthesis

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -4,4 +4,41 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  const textTyped = document.getElementById('text-to-speak');
+  const voiceSelect = document.getElementById('voice-select');
+  const speakButton = document.querySelector('button');
+  const smilingFace = document.querySelector('img');
+
+  const synth = window.speechSynthesis;
+  let voices = [];
+
+  function populateVoices() {
+    voices = synth.getVoices();
+    voices.forEach(function (voice, index) {
+      const option = document.createElement('option');
+      option.textContent = `${voice.name} (${voice.lang})`;
+      option.value = index;
+      voiceSelect.appendChild(option);
+    });
+  }
+
+  populateVoices();
+
+  // add event listeners
+  speakButton.addEventListener('click', function () {
+    const speech = new SpeechSynthesisUtterance(textTyped.value);
+    const selectedIndex = voiceSelect.value;
+
+    speech.voice = voices[selectedIndex];
+
+    speech.onstart = function () {
+      smilingFace.src = 'assets/images/open.png';
+    };
+
+    speech.onend = function () {
+      smilingFace.src = 'assets/images/smiling.png';
+    };
+
+    synth.speak(speech); 
+  });
 }
